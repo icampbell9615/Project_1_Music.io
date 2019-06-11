b3e5b9005cc6e0fe906f93ef3d381be8dfde1802
Campbell,Ian <icampbell9615@gmail.com>
2019-06-11 18:53:16 -0500

    commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+function userArtist(userArtist){
+    var APIKey = "6bd260e771ce2f1a6e020fe365f4537c";
+    var userArtist = "tupac"
+    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + userArtist + "&api_key=6bd260e771ce2f1a6e020fe365f4537c&format=json";
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+    
+      console.log(response);
+   
+        });
+       }
+        // Event handler for user clicking the select-artist button
+      $("#select-artist").on("click", function(event) {
+        // Preventing the button from trying to submit the form
+        event.preventDefault();
+        // Storing the artist name
+        var inputArtist = $("#artist-input").val().trim();
+    
+        // Running the searchBandsInTown function(passing in the artist as an argument)
+        userArtist(inputArtist);
+      });
