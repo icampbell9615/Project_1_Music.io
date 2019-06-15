125f5951997005d2bac92ca679d158b84ea095ba
Campbell,Ian <icampbell9615@gmail.com>
2019-06-15 10:59:59 -0500

    commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,54 @@
 function userArtist(userArtist){
     var APIKey = "6bd260e771ce2f1a6e020fe365f4537c";
-    var userArtist = "tupac"
     var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + userArtist + "&api_key=6bd260e771ce2f1a6e020fe365f4537c&format=json";
+    
     
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
     
-      console.log(response);
-   
-        });
-       }
+        var tracks = response.toptracks.track
+        console.log(response);
+
+ // Constructing HTML containing the artist information
+ for (var i = 0; i < 1; i++) {
+ var artistName = tracks[0];
+ console.log(artistName);
+ var info = $("<div>")
+ $("#article-section").append(info).attr("id", "swag")
+
+ 
+ //  $("#swag").append(JSON.stringify(artistName));
+ var trackResponse = artistName.image[3]["#text"];
+ $("#swag").append(trackResponse);
+ 
+ var imgElement = $("<img>");
+ $(imgElement).attr("src", trackResponse);
+ $(imgElement).attr("alt", "No image found artist");
+ $("body").append(imgElement);
+ 
+ 
+ }
+//  var artistURL = $("<a>").attr("href", response.url).append(artistName);
+//  var artistImage = $("<img>").attr("src", response.thumb_url);
+//  var trackerCount = $("<h2>").text(response.tracker_count + "");
+//  var upcomingEvents = $("<h2>").text(response.upcoming_event_count + "");
+//  var goToArtist = $("<a>").attr("href", response.url).text("");
+
+ // Empty the contents of the artist-div, append the new artist content
+//  $("#artist-div").empty();
+//  $("#artist-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
+
+
+     
+   });
+ }
+    
+    
        
+
+
         // Event handler for user clicking the select-artist button
       $("#select-artist").on("click", function(event) {
 
@@ -24,7 +60,6 @@ function userArtist(userArtist){
     
         // Running the function(passing in the artist as an argument)
         userArtist(inputArtist);
-
-
+        
       });
 
